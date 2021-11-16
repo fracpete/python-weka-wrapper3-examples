c5ecc68110261f2c8e32aad71312c27299fae2f5
Peter Reutemann <fracpete@gmail.com>
2021-11-16 14:21:32 +1300

    can fall back on default datasets now

--- src/wekaexamples/classifiers/output_class_distribution.py
+++ src/wekaexamples/classifiers/output_class_distribution.py
@@ -12,8 +12,9 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 # output_class_distribution.py
-# Copyright (C) 2014 Fracpete (pythonwekawrapper at gmail dot com)
+# Copyright (C) 2014-2021 Fracpete (pythonwekawrapper at gmail dot com)
 
+import os
 import sys
 import traceback
 import weka.core.jvm as jvm
@@ -26,17 +27,24 @@ def main(args):
     """
     Trains a J48 classifier on a training set and outputs the predicted class and class distribution alongside the
     actual class from a test set. Class attribute is assumed to be the last attribute.
-    :param args: the commandline arguments (train and test datasets)
+    :param args: the commandline arguments (train and test datasets, optional)
     :type args: list
     """
 
     # load a dataset
-    helper.print_info("Loading train: " + args[1])
+    if len(args) < 3:
+        helper.print_info("No custom dataset filenames provided, falling back on default ones")
+        train_file = helper.get_data_dir() + os.sep + "iris-train.arff"
+        test_file = helper.get_data_dir() + os.sep + "iris-test.arff"
+    else:
+        train_file = args[1]
+        test_file = args[2]
+    helper.print_info("Loading train: " + train_file)
     loader = Loader(classname="weka.core.converters.ArffLoader")
-    train = loader.load_file(args[1])
+    train = loader.load_file(train_file)
     train.class_index = train.num_attributes - 1
-    helper.print_info("Loading test: " + args[2])
-    test = loader.load_file(args[2])
+    helper.print_info("Loading test: " + test_file)
+    test = loader.load_file(test_file)
     test.class_is_last()
 
     # classifier
